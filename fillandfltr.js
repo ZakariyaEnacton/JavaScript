68a43cc7e60259775c3676ea9cb98b565d75c1bb
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-18 16:28:36 +0530

    feat : fill and filter of an array

---
+++ fillandfltr.js
@@ -0,0 +1,13 @@
+var value = [2, 5, 9, 6, 7, 3];
+
+// fill is changes the value in array
+
+console.log(value.fill('z', 2, 4)) // fill (value,strtIndex,endIndex(exclusive) 
+
+// filter use to filtering the element from array and give the new array
+
+var myValue = [22, 88, 5, 56, 54, 54, 32, 23, 15,]
+
+var result = myValue.filter((num) => num < 23);
+console.log(result);
+
