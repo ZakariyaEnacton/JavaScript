40f127c04c7e143958d07e2a98ae9f10c7bc2d66
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-18 14:55:42 +0530

    feat : array

---
+++ array.js
@@ -0,0 +1,24 @@
+var countries = ['India', 'USA', 'Russia', 'Enngland'];
+var states = new Array('Gujarat', 'Mumbai', 'Delhi', 'Banglore');
+
+// console.log(states[3]);
+// console.log(states.length);
+// states[2] = 'Punjab';
+// console.log(states.push('Haryana'));
+// states.unshift("Hydrabad"); // adding value at the first in the array
+// console.log(states);
+// states.shift(); // delete vaute at the first in the array
+// console.log(states);
+// states.pop();
+// console.log(states);
+
+var user = ["EnactOn", "enacton@gmail.com", 2, 5, true];
+user.pop()
+user.pop()
+console.log(user)
+
+console.log(user.indexOf('EnactOn'));
+
+console.log(Array.from("EnactOn"));
+
+
