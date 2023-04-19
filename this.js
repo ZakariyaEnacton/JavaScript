fe59ea2f4d2962cb646c52402982f8be91a62028
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-19 13:34:58 +0530

    feat : this

--- this.js
+++ this.js
@@ -1,30 +1,18 @@
-// console.log(this);
+console.log(this);
 
-// var info = {
-//     userName: 'JavaScript',
-//     userId: 123456,
-//     getUserDetails: function () {
-//         console.log('line 7', this);
-//         function sayHello() {
-//             console.log('Hello');
-//             console.log('line 10', this);
-//         }
-//         sayHello();
-//     }
-// }
-
-// info.getUserDetails();
-
-let count = 0
-
-var time = setInterval(() => {
-    if (count < 1000) {
-        count++
-        console.log(count);
+var info = {
+    userName: 'JavaScript',
+    userId: 123456,
+    getUserDetails: function () {
+        console.log('line 7', this);
+        function sayHello() {
+            console.log('Hello');
+            console.log('line 10', this);
+        }
+        sayHello();
     }
-}, 1);
+}
 
-setTimeout(() => {
-    console.log('Done...')
-}, 1600);
+info.getUserDetails();
+
 
