e64786059771d3131b3c004792c2524f17449092
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-19 11:59:31 +0530

    feat : while

---
+++ while.js
@@ -0,0 +1,27 @@
+var countries = [
+    'India',
+    'USA',
+    'England',
+    'Thiland',
+    2000,
+    'China',
+    'Japan',
+    2001,
+    'Singapore',
+]
+
+// let i = 0;
+
+// while (i < countries.length) {
+//     console.log(countries[i]);
+//     i++
+
+// }
+
+let i = 20;
+
+do {
+    console.log(i);
+    i++
+
+} while (i < 10)
