1d39c5fc5da3b1dbcc29abbfc804de2900eee1ad
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-19 10:30:39 +0530

    feat :

--- sliceandsplice.js
+++ sliceandsplice.js
@@ -1,6 +1,6 @@
 var user = ["Jhon", "David", "Tim", "Sam", "Mitchel"];
 
-// console.log(user.slice(2, 4)); // splice(strtIndex,endIndex(exclusive))
+console.log(user.slice(2, 4)); // splice(strtIndex,endIndex(exclusive))
 
 // here take the value from index one to index three and change with "Hello"  o/p : ['Jhon','Hello','Mitchel'] 
 user.splice(1, 3, "Hello") // splice(index, count, value) 
