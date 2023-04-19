a4f393479e47706303658cdf3478b0eb7bb96151
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-19 12:07:40 +0530

    feat : forEach loop

---
+++ foreach.js
@@ -0,0 +1,15 @@
+var countries = [
+    'India',
+    'USA',
+    'England',
+    'Thiland',
+    2000,
+    'China',
+    'Japan',
+    2001,
+    'Singapore',
+]
+
+countries.forEach((c) => (console.log(c))); // callback function with round braces
+
+countries.forEach(c => console.log(c)); // callback function without braces
