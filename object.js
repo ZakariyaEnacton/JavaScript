ce5c1c8bdd6200b1002971a3defbb26eeda7b637
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-19 10:30:17 +0530

    feat : object

---
+++ object.js
@@ -0,0 +1,10 @@
+var device = {
+    name: 'iPhone',
+    modelName: 'iPhone 12',
+    version: 16.2,
+    region: 'India',
+    price: 85000,
+    modelNumber: 'MGJE3HNJA'
+}
+
+console.table(device);
