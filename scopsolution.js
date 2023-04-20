32906e696d2e13d44358fc181f7238654087d396
ZakariyaEnacton <zakriyabombaywala@gmail.com>
2023-04-20 14:59:43 +0530

    feat : solution of scope

---
+++ scopsolution.js
@@ -0,0 +1,10 @@
+// console.log(name);
+
+var name = "JavaScript";
+
+if (true) {
+  var firstName = "Zakariya";
+  let lastName = "bombaywala"; // let don't allow to call lastName outside the scope
+}
+console.log(firstName);
+console.log(lastName);
