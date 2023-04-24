80d2c2385651a18e023cd127ffc4948b566b0c9c
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-24 11:51:06 +0530

    feat : spread and rest

--- spreadandrest.js
+++ spreadandrest.js
@@ -11,3 +11,17 @@ console.log(`this is call using array var : ` + sumTwo(mySum)); // using with ar
 // note: spread Op use to convert any object into seperate variables
 console.log(`this is using with spread operator : ` + sumTwo(...mySum)); // spread Operator
 
+
+// note: rest Op rest Op convert individual values into array
+
+function sumThree(a, b, ...args) { // rest Op
+    // console.log(args);
+    let sum = a + b;
+    let mul = 1;
+    for (const arg of args) {
+        mul *= arg;
+    }
+    return [mul, sum];
+}
+
+console.log(sumThree(5, 5, 5, 5));
