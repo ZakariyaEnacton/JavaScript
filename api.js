e46852435f56071d14d30d02bfcea40c8cf7bd9f
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-24 16:36:09 +0530

    feat : api call

---
+++ api.js
@@ -0,0 +1,8 @@
+fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(JSON.parse(data));
+        var joke = data.value;
+        console.log(joke);
+    })
+    .catch()
