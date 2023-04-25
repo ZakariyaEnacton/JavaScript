2d55769bfa9f3172491aea487bbd0b73c1076819
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-04-25 11:48:46 +0530

    feat : destructuring

---
+++ destructuring.js
@@ -0,0 +1,14 @@
+// function sum(v1, v2, v3) {
+//     return v1 + v2 + v3
+// }
+// console.log(sum(...[5, 6, 8]));
+
+
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+
+// arr3 = arr1.concat(arr2)
+arr3 = [...arr1, ...arr2]
+console.log(arr3);
+
+
